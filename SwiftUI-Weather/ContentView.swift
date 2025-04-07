d7c96aff51f26b86a3ec2312e744f95aecf7bf73
Lupey1103 <luperon.christian95@gmail.com>
2025-04-07 00:30:25 -0400

    Finished UI for the weather app

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -9,16 +9,79 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+			LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
+						   startPoint: .topLeading,
+						   endPoint: .bottomTrailing)
+				.edgesIgnoringSafeArea(.all)
+			VStack {
+				Text("Hialeah, FL")
+					.font(.system(size: 32, weight: .medium, design: .default))
+					.foregroundColor(.white)
+					.padding()
+				VStack(spacing: 8) {
+					Image(systemName: "cloud.sun.fill")
+						.renderingMode(.original)
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+						.frame(width: 180, height: 180)
+					
+					Text("76°")
+						.font(.system(size: 70, weight: .medium))
+						.foregroundColor(.white)
+				}
+				HStack(spacing: 10) {
+					WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temp: 74)
+					WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temp: 90)
+					WeatherDayView(dayOfWeek: "THU", imageName: "cloud.heavyrain.fill", temp: 82)
+					WeatherDayView(dayOfWeek: "FRI", imageName: "hurricane", temp: 98)
+					WeatherDayView(dayOfWeek: "SAT", imageName: "cloud.bolt.rain.fill", temp: 72)
+					
+				}
+				Spacer()
+				
+				Button {
+					print("tapped")
+				} label: {
+					Text("Change Day Time!!")
+						.frame(width: 280, height: 50)
+						.background()
+						.font(.system(size: 20, weight: .bold, design: .default))
+						.cornerRadius(10)
+				}
+				
+				Spacer()
+			}
         }
-        .padding()
+        
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct WeatherDayView: View {
+	
+	var dayOfWeek: String
+	var imageName: String
+	var temp: Int
+	
+	var body: some View {
+		VStack {
+			Text(dayOfWeek)
+				.font(.system(size: 16, weight: .medium, design: .default))
+				.foregroundColor(.white)
+				.padding()
+			Image(systemName: imageName)
+				.renderingMode(.original)
+				.resizable()
+				.aspectRatio(contentMode: .fit)
+				.frame(width: 40, height: 40)
+			Text("\(temp)°")
+				.font(.system(size: 28, weight: .medium))
+				.foregroundColor(.white)
+		}
+		
+	}
 }
