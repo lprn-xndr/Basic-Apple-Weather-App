31fd24cd056c8b5373259f57f74a14ca609acb82
Lupey1103 <luperon.christian95@gmail.com>
2025-04-10 02:43:21 -0400

    Refactored HStack with an array

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -17,14 +17,7 @@ struct ContentView: View {
 			VStack {
 				CityTextView(cityName: "Hialeah, FL")
 				MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temp: 76)
-				HStack(spacing: 10) {
-					WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temp: 74)
-					WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temp: 90)
-					WeatherDayView(dayOfWeek: "THU", imageName: "cloud.heavyrain.fill", temp: 82)
-					WeatherDayView(dayOfWeek: "FRI", imageName: "hurricane", temp: 98)
-					WeatherDayView(dayOfWeek: "SAT", imageName: "cloud.bolt.rain.fill", temp: 72)
-					
-				}
+				WeeklyView()
 				Spacer()
 				
 				Button {
@@ -111,3 +104,19 @@ struct MainWeatherStatusView: View {
 	}
 }
 
+
+struct WeeklyView: View {
+	
+	var days : [String] = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+	
+	var body: some View {
+		HStack(spacing: 10) {
+			WeatherDayView(dayOfWeek: days[1], imageName: "cloud.sun.fill", temp: 74)
+			WeatherDayView(dayOfWeek: days[2], imageName: "sun.max.fill", temp: 90)
+			WeatherDayView(dayOfWeek: days[3], imageName: "cloud.heavyrain.fill", temp: 82)
+			WeatherDayView(dayOfWeek: days[4], imageName: "hurricane", temp: 98)
+			WeatherDayView(dayOfWeek: days[5], imageName: "cloud.bolt.rain.fill", temp: 72)
+			
+		}
+	}
+}
